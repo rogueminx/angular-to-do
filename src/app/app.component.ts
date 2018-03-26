c261565073deb99e99f3fef001131f650f891f07
Anna Marie Cooper and Maggie Summers <anna+maggie@>
2018-03-26 14:56:00 -0700

    add a edit Task functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,24 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
+  selectedTask: null;
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+  priorityColor(currentTask){
+   if (currentTask.priority === 3){
+     return "bg-danger";
+   } else if (currentTask.priority === 2) {
+     return  "bg-warning";
+   } else {
+     return "bg-info";
+   }
+ }
+ finishedEditing() {
+   this.selectedTask = null;
+ }
 }
