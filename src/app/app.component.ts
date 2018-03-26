ebd1f94b2ad2a4ce963f52c4816ce0d8a33ea140
Anna Marie Cooper and Maggie Summers <maggie@>
2018-03-26 09:57:58 -0700

    build basic to-do app with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'crazy-app-selector',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] // array of link to various stylesheets
 })
+
 export class AppComponent {
-  title = 'app';
+  currentTask: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course'),
+    new Task('Begin brainstorming possible JavaScript group projects'),
+    new Task('Add README file to last few Angular repos on GitHub')
+  ];
 }
